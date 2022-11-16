3425b758e5df04222a6c685e87fe984cfef1e5c1
lmarchiorit <98050866+lmarchiorit@users.noreply.github.com>
2022-11-16 16:10:57 +0000

    Update Home.jsx
    I am not sure if that's how I should've done it. I realized I cannot use "'" so there might be some spelling errors.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ function Home() {
     // This needs re-doing completely to be the landing/home page that explains the app and it's purpose.
     // Nearly all of the code below should be deleted and replaced with a cool looking page!
  <>
- <h1>Page title</h1>
- <p className="lead">Some intro text here that will grab attention/explain the site...</p>
- <Alert variant="danger">Here we can use this element to add the warning about needing to attend court maybe?</Alert>
+ <h1>Eviction Support will help you to check if you are elegible for legal aid for your housing issue.</h1>
+ <p className="lead">If you have a court case, Eviction Support will help you to check if you are elegible for legal aid for your housing matter. The website also create a report with your informations so you can download it on your device and take it to your solicitor or to court in case you dont find a solicitor.</p>
+ <Alert variant="danger">Please note: It is very important that you have your case heard, so DO NOT MISS YOUR COURT DATE. use the websites report to show the Judge/Duty Solicitor on site about your case and have your side heard!</Alert>
 
     </>
   );
